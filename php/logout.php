5d87fc5573a776606a472abb06665afa48c8fd36
Moh. Ilham Burhanuddin <m.ilham.b.97@gmail.com>
2021-12-04 00:05:25 +0700

    implementasi JWT menggunakan library php JWT

--- php/logout.php
+++ php/logout.php
@@ -1,3 +1,6 @@
 <?php
+require_once __DIR__.'/vendor/autoload.php';
+
+setcookie("X-76c-Session", "LOGOUT");
 header('Location: /login.php');
 ?>
--- php/src/session.php
+++ php/src/session.php
@@ -1,9 +1,10 @@
 <?php
+use Firebase\JWT\JWT;
 
 class Session {
     public $username;
     public $role;
-    
+
     public function __construct(string $username, string $role)
     {
         $this->username = $username;
@@ -12,11 +13,19 @@ class Session {
 }
 
 class SessionManager {
+    private static $SECRET_KEY = "contohkey123";
+
     public static function login(string $username, string $password) : bool
     {
         if ($username == "ilham" && $password == "ilham") {
-            $_SESSION['username'] = $username;
-            $_SESSION['role'] = 'customer';
+            $payload = [
+                "username" => $username,
+                "role" => "customer"
+            ];
+
+            $jwt = JWT::encode($payload, SessionManager::$SECRET_KEY, "HS256");
+            setcookie("X-76c-Session", $jwt);
+
             return true;
         } else {
             return false;
@@ -25,10 +34,19 @@ class SessionManager {
 
     public static function getCurrentSession(): Session
     {
-        if ($_SESSION['username']) {
-            return new Session($_SESSION['username'], $_SESSION['role']);
-        } else {
-            throw new Exception("User is not login");
+        try {
+            if ($_COOKIE['X-76c-Session']) {
+                $jwt = $_COOKIE['X-76c-Session'];
+                $payload = JWT::decode($jwt, SessionManager::$SECRET_KEY, ['HS256']);
+
+                // library JWT akan mengecek JWT valid atau tidak
+                // jika tidak valid maka akan throw Exception
+                return new Session($payload->username, $payload->role);
+            } else {
+                throw new Exception("User is not login");
+            }
+        } catch (Exception $e) {
+            throw new Exception("User is notlogin");
         }
     }
 }
