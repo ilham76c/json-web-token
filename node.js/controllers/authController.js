8a3b66426805c706bc2939a67a521c55993b85c7
Moh. Ilham Burhanuddin <m.ilham.b.97@gmail.com>
2023-04-03 16:17:49 +0700

    Refactor: add try catch

--- node.js/controllers/authController.js
+++ node.js/controllers/authController.js
@@ -21,7 +21,7 @@ const handleLogin = async (req, res) => {
         const accessToken = jwt.sign(
             { "username": foundUser.username },
             process.env.ACCESS_TOKEN_SECRET,
-            { expiresIn: '30s' }
+            { expiresIn: '60s' }
         );
         const refreshToken = jwt.sign(
             { "username": foundUser.username },
--- node.js/middleware/verifyJWT.js
+++ node.js/middleware/verifyJWT.js
@@ -2,19 +2,25 @@ const jwt = require('jsonwebtoken');
 require('dotenv').config();
 
 const verifyJWT = (req, res, next) => {
-    const authHeader = req.headers['authorization'];
-    if (!authHeader) return res.sendStatus(401);
-    console.log(authHeader); // Bearer token
-    const token = authHeader.split(' ')[1];
-    jwt.verify(
-        token,
-        process.env.ACCESS_TOKEN_SECRET,
-        (err, decoded) => {
-            if (err) return res.sendStatus(403); //invalid token
-            req.user = decoded.username;
-            next();
-        }
-    );
+    try {
+        const authHeader = req.headers['authorization'];
+        if (!authHeader) return res.sendStatus(401);
+
+        console.log(authHeader); // Format token: Bearer token
+        const token = authHeader.split(' ')[1];
+
+        jwt.verify(
+            token,
+            process.env.ACCESS_TOKEN_SECRET,
+            (err, decoded) => {
+                if (err) return res.sendStatus(403); // invalid token
+                req.user = decoded.username;
+                next();
+            }
+        );
+    } catch (err) {
+        console.log(err);
+    }
 }
 
 module.exports = verifyJWT
